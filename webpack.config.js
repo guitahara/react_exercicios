bea2c3b60259090ba48f0483c078328399953808
guilherme.benedeti@fcamara.com.br <guilherme.benedeti@fcamara.com.br>
2019-01-21 18:54:55 -0200

    início do projeto

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './ex/index.jsx',
+    output: {
+        path: __dirname+'/public',
+        filename: './bundle.js'
+    },
+    devServer: {
+        port: 8080,
+        contentBase: './public'
+    },
+    module: {
+        rules:[{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: 'babel-loader',
+                options: {
+                   presets: ['@babel/preset-env','@babel/preset-react']
+                }
+            }]
+        }]
+    }
+}
